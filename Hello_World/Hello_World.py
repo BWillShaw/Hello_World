1d8eda10feb5f2a2eb19a1ecee7e1149002f0dcb
KozmoP <108187800+BSSD3980@users.noreply.github.com>
2023-09-12 23:22:50 +0100

    Updated 'Hello_World' to print chunky letters

--- Hello_World/Hello_World.py
+++ Hello_World/Hello_World.py
@@ -1,2 +1,66 @@
 
-print("Hello World!")
+print("Hello World!\n\n")
+
+def print_chunky_letter(letter):
+    if letter == " ":
+        print("      ")
+        return
+
+    if letter == "H":
+        print("HH  HH")
+        print("HH  HH")
+        print("HHHHHH")
+        print("HH  HH")
+        print("HH  HH")
+
+    elif letter == "E":
+        print("EEEEE")
+        print("E    ")
+        print("EEEEE")
+        print("E    ")
+        print("EEEEE")
+
+    elif letter == "L":
+        print("L    ")
+        print("L    ")
+        print("L    ")
+        print("L    ")
+        print("LLLLL")
+
+    elif letter == "O":
+        print(" OOO ")
+        print("O   O")
+        print("O   O")
+        print("O   O")
+        print(" OOO ")
+
+    elif letter == "W":
+        print("\n")
+        print("W   W")
+        print("W   W")
+        print("W W W")
+        print("WW WW")
+        print("W   W")
+
+    elif letter == "R":
+        print("RRRR ")
+        print("R   R")
+        print("RRRR ")
+        print("R  R ")
+        print("R   R")
+
+    elif letter == "D":
+        print("DDD  ")
+        print("D   D")
+        print("D   D")
+        print("D   D")
+        print("DDD  ")
+
+    print()
+
+
+def print_chunky_text(text):
+    for letter in text.upper():
+        print_chunky_letter(letter)
+
+print_chunky_text("Hello World")
